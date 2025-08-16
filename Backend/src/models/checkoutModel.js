10d88e81a20eee48811593c622f1f5254b969e83
Lucas Avilés <lbaviles2@gmail.com>
2025-08-15 22:12:32 -0400

    feat: se agregan rutas, modelos y controladores de games y checkout integrados a supabase

--- Backend/src/models/checkoutModel.js
+++ Backend/src/models/checkoutModel.js
@@ -1,20 +1,30 @@
-import db from '../config/db.js';
+import { supabase } from '../config/supabase.js'
 
 export const getAllCheckouts = async () => {
-  const result = await db.query('SELECT * FROM checkout');
-  return result.rows;
-};
+  const { data, error } = await supabase.from('checkout').select('*')
+  if (error) throw error
+  return data
+}
 
 export const createCheckout = async (checkout) => {
   const {
     user_id, nombre, direccion, ciudad, codigo_postal, telefono, tarjeta, expiracion, cvv
-  } = checkout;
+  } = checkout
 
-  const result = await db.query(
-    `INSERT INTO checkout (user_id, nombre, direccion, ciudad, codigo_postal, telefono, tarjeta, expiracion, cvv)
-     VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *`,
-    [user_id, nombre, direccion, ciudad, codigo_postal, telefono, tarjeta, expiracion, cvv]
-  );
+  const { data, error } = await supabase.from('checkout').insert([
+    {
+      user_id,
+      nombre,
+      direccion,
+      ciudad,
+      codigo_postal,
+      telefono,
+      tarjeta,
+      expiracion,
+      cvv
+    }
+  ]).select().single()
 
-  return result.rows[0];
-};
+  if (error) throw error
+  return data
+}
