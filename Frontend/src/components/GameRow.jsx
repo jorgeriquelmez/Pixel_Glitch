169fb2c4df80f7f058d5fd74461fced6281b125f
jorge riquelme <jorge.riquelme.z@gmail.com>
2025-08-23 19:22:16 -0400

    cambios gameow

--- Frontend/src/components/GameRow.jsx
+++ Frontend/src/components/GameRow.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 /**
  * GameRow
  * Props:
- *  - idx: índice en el array
- *  - juego: { nombre, plataforma, precio, imagen }
- *  - onDelete: función para borrar este juego
+ * - idx: índice en el array
+ * - juego: { id, title, platforms, price, image, genre, release_date, popularity }
+ * - onDelete: función para borrar este juego
+ * - onEdit: función para editar este juego
  */
-export default function GameRow({ idx, juego, onDelete }) {
+export default function GameRow({ idx, juego, onDelete, onEdit }) {
   return (
     <tr>
       <td>{idx + 1}</td>
-      <td>{juego.nombre}</td>
-      <td>{juego.plataforma}</td>
-      <td>${juego.precio}</td>
+      <td>{juego.title}</td>
+      <td>{juego.platforms}</td>
+      <td>${juego.price}</td>
       <td>
-        <button className="btn-edit">Editar</button>
+        <button className="btn-edit" onClick={onEdit}>
+          Editar
+        </button>
         <button className="btn-delete" onClick={onDelete}>
           Eliminar
         </button>
       </td>
     </tr>
-  )
+  );
 }
